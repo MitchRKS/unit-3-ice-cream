b7f7b5629a0f9938b5910c6ad5c7807ef2045cbc
Gowrishankar Janakiraman <jgowris@gmail.com>
2023-03-15 01:54:07 -0700

    Updated Pages

--- src/Pages/ParlorMenu.jsx
+++ src/Pages/ParlorMenu.jsx
@@ -6,15 +6,79 @@ import React from "react";
 
 export default function ParlorMenu() {
   return (
-    <div>
+    <div className="eachItem">
       <div className="menuToOrder">
         <p>Ice cream name goes here</p>
         <p>Ice Cream description goes here</p>
-        <p>Size: Like it, Love it, Have it all</p>
-        <p>
-          Toppings: Nuts, crushed almonds, sprinkle, caramel, chocoloate,
-          peanuts, jelly, tooty fruity
-        </p>
+        <p class="priceDisp">$12.75</p>
+        <fieldset>
+          <legend>Choose a size</legend>
+          <div>
+            <input type="checkbox" id="likeIt" name="size" value="size" />
+            <label for="likeIt">Like it</label>
+          </div>
+          <div>
+            <input type="checkbox" id="loveIt" name="size" value="size" />
+            <label for="loveIt">Love it</label>
+          </div>
+          <div>
+            <input type="checkbox" id="haveItAll" name="size" value="size" />
+            <label for="haveItAll">Have it all</label>
+          </div>
+        </fieldset>
+
+        <fieldset>
+          <legend>Choose Toppings</legend>
+          {/* <div>
+            <input type="checkbox" id="nuts" name="toppings" value="toppings" />
+            <label for="nuts">Nuts</label>
+          </div> */}
+          <div>
+            <input
+              type="checkbox"
+              id="chocoChips"
+              name="toppings"
+              value="toppings"
+            />
+            <label for="chocoChips">Choco chips</label>
+          </div>
+          <div>
+            <input
+              type="checkbox"
+              id="caramel"
+              name="toppings"
+              value="toppings"
+            />
+            <label for="caramel">Caramel</label>
+          </div>
+          <div>
+            <input
+              type="checkbox"
+              id="MandMs"
+              name="toppings"
+              value="toppings"
+            />
+            <label for="MandMs">M & M</label>
+          </div>
+          <div>
+            <input
+              type="checkbox"
+              id="Sprinkle"
+              name="toppings"
+              value="toppings"
+            />
+            <label for="Sprinkle">Sprinkle`</label>
+          </div>
+          <div>
+            <input
+              type="checkbox"
+              id="peanuts"
+              name="toppings"
+              value="toppings"
+            />
+            <label for="peanuts">Peanuts</label>
+          </div>
+        </fieldset>
       </div>
     </div>
   );
