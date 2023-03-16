758328b13305ab44b0cb801d36dc88b2e0b9461b
Hilal Yilmaz <hilaltiftik@gmail.com>
2023-03-16 07:40:39 -0700

    create utilities for user authentication

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,10 @@ import AuthPage from "./Pages/AuthPage";
 import ParlorMenu from "./Pages/ParlorMenu";
 import ParlorOrder from "./Pages/ParlorOrder";
 import NavBar from "./components/NavBar";
+import { getUser } from "../src/utilities/users-service";
 
 function App() {
-  const [user, setUser] = useState({});
+  const [user, setUser] = useState(getUser());
 
   return (
     <main className="App">
@@ -17,15 +18,15 @@ function App() {
           <Routes>
             {/* IF THE USER IS AUTHENTICATED SHOW THE MENU PAGE FOR THE USER TO
           INTIATE THE ORDER  */}
-            <Route path="/orders/new" element={<ParlorMenu />} />
+            <Route path="/orders/new" element={<ParlorMenu user={user} setUser={setUser} />} />
             {/* THIS PAGE WILL SHOW THE ORDER DETAILS OF THE CURRENT ORDER
           (SHOPPING CART) */}
-            <Route path="/orders" element={<ParlorOrder />} />
+            <Route path="/orders" element={<ParlorOrder user={user} setUser={setUser} />} />
           </Routes>
         </>
       ) : (
         /* IF THE USER IS UNAUTHENTICATED SHOW THE LOGIN/SIGNUP PAGE */
-        <AuthPage />
+        <AuthPage setUser={setUser} />
       )}
     </main>
   );
