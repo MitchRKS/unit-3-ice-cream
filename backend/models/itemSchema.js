20c6a6747ab35cffcccbcbaafb80c26422b9ff24
Mitch Rucker <mitchruckerks@gmail.com>
2023-03-16 22:21:29 -0500

    Add values to itemschema to match front end

--- backend/models/itemSchema.js
+++ backend/models/itemSchema.js
@@ -6,6 +6,9 @@ const itemSchema = new Schema(
   {
     name: { type: String, required: true },
     category: { type: Schema.Types.ObjectId, ref: "Category" },
+    size: { type: String, required: true },
+    flavor: { type: String, required: true },
+    toppings: { type: Number, required: false, default: 0 },
     price: { type: Number, required: true, default: 0 },
   },
   {
