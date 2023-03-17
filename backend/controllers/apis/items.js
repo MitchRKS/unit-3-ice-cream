9d8301f9bb7f0f34836188c0e3d7fb87cbca9d26
Mitch Rucker <mitchruckerks@gmail.com>
2023-03-16 22:50:45 -0500

    Add items controller and orders controller to backend

---
+++ backend/controllers/apis/items.js
@@ -0,0 +1,25 @@
+const Item = require("../../models/item");
+
+async function index(req, res) {
+  try {
+    const items = await Item.find({}).sort("name").populate("category").exex();
+    items.sort((a, b) => a.category.sortOrder - b.category.sortOrder);
+    res.status(200).json(items);
+  } catch (error) {
+    res.status(400).json({ msg: error.message });
+  }
+}
+
+async function show(req, res) {
+  try {
+    const item = await Item.findById(req.params.id);
+    res.status(200).json(item);
+  } catch (error) {
+    res.status(400).json({ msg: error.message });
+  }
+}
+
+module.exports = {
+  index,
+  show,
+};
