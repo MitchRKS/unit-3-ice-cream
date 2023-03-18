7db01c17ca213ee06ebc980a9e3ee0e5a2760bbb
Mitch Rucker <mitchruckerks@gmail.com>
2023-03-18 12:10:01 -0500

    setup user model

--- backend/models/user.js
+++ backend/models/user.js
@@ -4,6 +4,8 @@ const Schema = mongoose.Schema;
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true },
   password: { type: String, required: true },
+  admin_access: { type: Boolean, default: false },
+  
 });
 
 module.exports = mongoose.model("User", userSchema);
