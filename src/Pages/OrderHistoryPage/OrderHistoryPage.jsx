e54e35d925d05d6482f384c9b798bd313176bf18
Gowrishankar Janakiraman <jgowris@gmail.com>
2023-03-18 16:45:47 -0700

    Updates in progress

--- src/Pages/OrderHistoryPage/OrderHistoryPage.jsx
+++ src/Pages/OrderHistoryPage/OrderHistoryPage.jsx
@@ -1,13 +1,49 @@
-//*************************************************************************
-// THIS PAGE WILL SHOW THE CURRENT USER ORDER ITEMS.
-//*************************************************************************
+import styles from "./OrderHistoryPage.module.scss";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import * as ordersAPI from "../../utilities/orders-api";
+import Logo from "../../components/Logo/Logo";
+import UserLogOut from "../../components/UserLogOut/UserLogOut";
+import OrderList from "../../components/OrderList/OrderList";
+import OrderDetail from "../../components/OrderDetail/OrderDetail";
+import Aside from "../../components/Aside";
 
-import React from "react";
+export default function OrderHistoryPage({ user, setUser }) {
+  /*--- State --- */
+  const [orders, setOrders] = useState([]);
+  const [activeOrder, setActiveOrder] = useState(null);
 
-export default function ParlorOrder() {
+  /*--- Side Effects --- */
+  useEffect(function () {
+    // Load previous orders (paid)
+    async function fetchOrderHistory() {
+      const orders = await ordersAPI.getOrderHistory();
+      setOrders(orders);
+      // If no orders, activeOrder will be set to null below
+      setActiveOrder(orders[0] || null);
+    }
+    fetchOrderHistory();
+  }, []);
+
+  /*--- Event Handlers --- */
+  function handleSelectOrder(order) {
+    setActiveOrder(order);
+  }
+
+  /*--- Rendered UI --- */
   return (
-    <div>
-      <h1>Your Order Details</h1>
-    </div>
+    <main className={styles.OrderHistoryPage}>
+      <Aside user={user} setUser={setUser} styles={styles.aside}>
+        <Link to="/orders/new" className="button btn-sm">
+          NEW ORDER
+        </Link>
+      </Aside>
+      <OrderList
+        orders={orders}
+        activeOrder={activeOrder}
+        handleSelectOrder={handleSelectOrder}
+      />
+      <OrderDetail order={activeOrder} />
+    </main>
   );
 }
